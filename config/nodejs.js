4da52e299667a0d3325f29f1156301f38ad3eb06
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-10-20 14:07:29 -0230

    Disable node/no-unsupported-features/es-syntax (#71)
    This rule does not offer support for ES2015 import/export syntax (ESM) which is
    supported by many of the other rules. Ensuring no invalid syntax is used is best
    left to the build tools/runtime.

--- config/nodejs.js
+++ config/nodejs.js
@@ -21,7 +21,7 @@ module.exports = {
     'node/no-unpublished-import': 'error',
     'node/no-unpublished-require': 'error',
     'node/no-unsupported-features/es-builtins': 'error',
-    'node/no-unsupported-features/es-syntax': 'error',
+    'node/no-unsupported-features/es-syntax': 'off',
     'node/no-unsupported-features/node-builtins': 'error',
     'node/process-exit-as-throw': 'error',
     'node/shebang': 'error',
