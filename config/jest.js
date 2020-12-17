792ef45f1fbb13c1618f40269a9ef7484b8a0c42
Erik Marks <rekmarks@protonmail.com>
2020-12-17 11:34:29 -0800

    More rules updates

--- config/jest.js
+++ config/jest.js
@@ -5,5 +5,36 @@ module.exports = {
   env: {
     'jest/globals': true,
   },
-  extends: ['plugin:jest/recommended'],
+  extends: [
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+  ],
+  rules: {
+    'jest/consistent-test-it': ['error', { 'fn': 'it' }],
+    'jest/lowercase-name': ['error', { 'ignore': ['describe'] }],
+    'jest/no-duplicate-hooks': 'error',
+    'jest/no-hooks': 'off',
+    'jest/no-if': 'error',
+    'jest/no-large-snapshots': 'off',
+    'jest/no-test-return-statement': 'error',
+    'jest/prefer-called-with': 'off',
+    'jest/prefer-expect-assertions': 'off',
+    'jest/prefer-hooks-on-top': 'error',
+    'jest/prefer-spy-on': 'error',
+    'jest/prefer-strict-equal': 'error',
+    'jest/prefer-todo': 'error',
+    'jest/require-top-level-describe': 'error',
+    'jest/require-to-throw-message': 'error',
+    'jest/valid-expect': ['error', { 'alwaysAwait': true }],
+    'jest/no-restricted-matchers': [
+      'error',
+      {
+        'resolves': 'Use `expect(await promise)` instead.',
+        'toBeFalsy': 'Avoid `toBeFalsy`',
+        'toBeTruthy': 'Avoid `toBeTruthy`',
+        'toMatchSnapshot': 'Use `toMatchInlineSnapshot()` instead',
+        'toThrowErrorMatchingSnapshot': 'Use `toThrowErrorMatchingInlineSnapshot()` instead',
+      },
+    ],
+  },
 }
