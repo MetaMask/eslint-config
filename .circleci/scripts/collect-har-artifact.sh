bbd3eb7814b6395ebe3d0403cc65a61c0bd7efd2
Erik Marks <rekmarks@protonmail.com>
2021-03-29 14:42:19 -0700

    Update CircleCI scripts, gitignore

--- .circleci/scripts/collect-har-artifact.sh
+++ .circleci/scripts/collect-har-artifact.sh
@@ -6,4 +6,11 @@ set -u
 set -o pipefail
 
 mkdir -p build-artifacts/yarn-install-har
-mv ./*.har build-artifacts/yarn-install-har/
+
+cd ./packages
+
+# ref: https://github.com/koalaman/shellcheck/wiki/SC2044
+find . -mindepth 1 -maxdepth 1 -type d -exec sh -c '
+    cd "$1"
+    mv ./*.har ../../build-artifacts/yarn-install-har/
+  ' sh {} \;
--- .circleci/scripts/deps-install.sh
+++ .circleci/scripts/deps-install.sh
@@ -5,4 +5,10 @@ set -e
 set -u
 set -o pipefail
 
-yarn --frozen-lockfile --ignore-scripts --har
+cd ./packages
+
+# ref: https://github.com/koalaman/shellcheck/wiki/SC2044
+find . -mindepth 1 -maxdepth 1 -type d -exec sh -c '
+    cd "$1"
+    yarn --frozen-lockfile --ignore-scripts --har
+  ' sh {} \;
