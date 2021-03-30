75e8cbe53e7751e7c05d435ca4b448ffd6b57731
Erik Marks <rekmarks@protonmail.com>
2021-03-30 14:58:16 -0700

    Add prettier rule validation script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,26 @@
 module.exports = {
   root: true,
 
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+
   extends: ['@metamask/eslint-config', '@metamask/eslint-config-nodejs'],
+
+  overrides: [
+    {
+      files: ['./scripts/*.js'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      rules: {
+        'import/no-dynamic-require': 'off',
+        'node/global-require': 'off',
+        'node/no-sync': 'off',
+        'node/no-unpublished-require': 'off',
+      },
+    },
+  ],
+
+  ignorePatterns: ['!.eslintrc.js'],
 };
