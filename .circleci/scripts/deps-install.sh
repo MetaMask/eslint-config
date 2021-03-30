9d9de52c0d87c3bc66d865fb0498f93df9eb8430
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2021-03-30 08:51:23 -0700

    Migrate to monorepo (#140)
    * Move existing package-specific files to new directory
    * lerna init
    * Add lerna as a dev dependency
    * Update CircleCI scripts for yarn v1 workspaces frozen lockfile bug
    * Remove package lockfile
    * Use module template gitignore

--- .circleci/scripts/deps-install.sh
+++ .circleci/scripts/deps-install.sh
@@ -5,4 +5,13 @@ set -e
 set -u
 set -o pipefail
 
-yarn --frozen-lockfile --ignore-scripts --har
+# --frozen-lockfile doesn't work in monorepos for yarn v1, so we diff ourselves
+# See: https://github.com/yarnpkg/yarn/issues/4098
+
+yarn --ignore-scripts --har
+
+if ! git diff --quiet;
+then
+  echo "Error: yarn install modified lockfile";
+  exit 1;
+fi
