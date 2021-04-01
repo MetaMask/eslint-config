5f126d5a48005495637ff825de5d0e5276c99b1a
Erik Marks <rekmarks@protonmail.com>
2021-04-01 10:15:12 -0700

    Sort flat rule keys, add docstrings

--- scripts/validate-rules.js
+++ scripts/validate-rules.js
@@ -32,6 +32,7 @@ async function main() {
   const prettierViolations = getConfigViolationsMap(metamaskConfigs);
   const snapshotViolations = [];
 
+  // Iterates over this monorepo's config packages and validates their rules.
   await Promise.all(
     Object.entries(metamaskConfigs).map(
       async ([packageName, { flatRules, packagePath }]) => {
@@ -41,7 +42,7 @@ async function main() {
           requiredPrettierRules,
           prettierViolations,
         );
-        await validateRulesSnapshot(
+        await validateOrWriteRulesSnapshot(
           packageName,
           packagePath,
           flatRules,
@@ -72,6 +73,13 @@ async function main() {
   process.exit(failures);
 }
 
+/**
+ * Assuming the given violations map contains violations, prints these in a
+ * readable format to console.error.
+ *
+ * @param {Record<string, string[]>} prettierViolations - A map containing
+ * Prettier violations.
+ */
 function logPrettierViolations(prettierViolations) {
   console.error(
     `\nError: Detected Prettier rule violations. Disable the specified rule(s) in the following package(s):\n\n${Object.entries(
@@ -84,6 +92,13 @@ function logPrettierViolations(prettierViolations) {
   );
 }
 
+/**
+ * Assuming the given violations map contains violations, prints these in a
+ * readable format to console.error.
+ *
+ * @param {Record<string, string[]>} snapshotViolations - A map containing
+ * snapshot violations.
+ */
 function logSnapshotViolations(snapshotViolations) {
   console.error(
     `\nError: Computed snapshot differs from the existing snapshot for the following package(s). Take a new snapshot and try again.\n\n${tabs(
@@ -92,12 +107,28 @@ function logSnapshotViolations(snapshotViolations) {
   );
 }
 
-function tabs(n) {
+/**
+ * @param {number} numTabs - The number of tabs to return.
+ * @returns {string} A string consisting of numTabs 4-space "tabs".
+ */
+function tabs(numTabs) {
   const TAB = '    ';
-  return TAB + new Array(n).join(TAB);
+  return TAB + new Array(numTabs).join(TAB);
 }
 
-async function validateRulesSnapshot(
+/**
+ * If the program is in write mode, writes a rule snapshot to the given path.
+ * Otherwise, checks whether the computed rules snapshot differs from the
+ * snapshot on disk, and stores an error in the violations map if so.
+ *
+ * Mutates the violations map in place.
+ *
+ * @param {string} packageName - The name of the config package.
+ * @param {string} packagePath - The path to the config package.
+ * @param {Record<string, unknown>} flatRules - The package's flattened rules.
+ * @param {Record<string, string[]>} violations - A map to store violations in.
+ */
+async function validateOrWriteRulesSnapshot(
   packageName,
   packagePath,
   flatRules,
@@ -126,6 +157,14 @@ async function validateRulesSnapshot(
   }
 }
 
+/**
+ * Writes a rules snapshot to the given path.
+ *
+ * Calls process.exit(1) if the write call fails.
+ *
+ * @param {string} snapshotFilePath - The path of the file to write.
+ * @param {Record<string, unknown>} flatRules - The rules to write.
+ */
 async function writeRulesSnapshot(snapshotFilePath, flatRules) {
   try {
     await fs.writeFile(
@@ -141,6 +180,17 @@ async function writeRulesSnapshot(snapshotFilePath, flatRules) {
   }
 }
 
+/**
+ * Checks whether the given package violates any of the given Prettier rules,
+ * and stores those violations in the given violations map.
+ *
+ * Mutates the violations map in place.
+ *
+ * @param {string} packageName - The name of the config package.
+ * @param {Record<string, unknown>} flatRules - The package's flattened rules.
+ * @param {Record<string, 'off' | unknown>} prettierRules - The required Prettier rules.
+ * @param {Record<string, string[]>} violations - A map to store violations in.
+ */
 function validatePrettierRules(
   packageName,
   flatRules,
@@ -155,7 +205,11 @@ function validatePrettierRules(
 }
 
 /**
- * Checks whether a config violations map has any violations.
+ * Checks whether a config violations map contains any violations.
+ *
+ * @param {Record<string, string[]>} - A map of package names to arrays with
+ * violated rules, if any.
+ * @returns {boolean} Whether the given map contains any violations.
  */
 function hasPrettierViolations(violationsMap) {
   for (const violations of Object.values(violationsMap)) {
@@ -170,6 +224,11 @@ function hasPrettierViolations(violationsMap) {
  * Takes a { [packageName]: any } map and returns a map with with the same keys
  * and empty array values, for storing rule violations that can be logged to the
  * console and fixed manually.
+ *
+ * @param {Record<string, unknown>} configs - An object with package name keys and arbitrary
+ * values.
+ * @returns {Record<string, string[]>} An object with the same keys and empty
+ * array values.
  */
 function getConfigViolationsMap(configs) {
   return Object.keys(configs).reduce((map, packageName) => {
@@ -183,7 +242,13 @@ function getConfigViolationsMap(configs) {
 // /
 
 /**
- * Gets the configs from all our packages, keyed by package names.
+ * Iterates over the packages in this monorepo and returns an object of package
+ * name keys with object values containing:
+ * - The raw config
+ * - Its flattened, complete rule set
+ * - The path to the package
+ *
+ * @returns {Record<string, Record<string, Object|string>} The config map.
  */
 function getMetamaskConfigs() {
   return readdirSync(PACKAGES_DIR_PATH).reduce((allConfigs, dirName) => {
@@ -206,13 +271,16 @@ function getMetamaskConfigs() {
 /**
  * Gets the rules that are always recommended to be disabled per Prettier's
  * ESLint config.
+ *
+ * @returns {string[]} The names of the rules that should always be
+ * disabled when using Prettier.
  */
 function getRequiredPrettierRules() {
   return Object.entries(getFlatRules(prettierConfig)).reduce(
     (allRules, [ruleName, ruleValue]) => {
       // Rules set to 'off' should never be enabled.
       // Rules set to 0 (number) may sometimes be included. We don't attend to those.
-      // https://github.com/prettier/eslint-config-prettier/blob/abf3ba1/index.js#L7-L9
+      // https://github.com/Prettier/eslint-config-Prettier/blob/abf3ba1/index.js#L7-L9
       if (ruleValue === OFF) {
         allRules.push(ruleName);
       }
@@ -226,10 +294,19 @@ function getRequiredPrettierRules() {
 // ESLint config parsing utilities.
 // /
 
+/**
+ * Takes an eslint config object and returns its own rules and the rules of its
+ * extended configs (if any) in a single, flat object.
+ *
+ * @param {Record<string, unknown>} configObject - An eslint config object (e.g. .eslintrc.js).
+ * @returns {Record<string, unknown>} An object of eslint rule names and their
+ * configuration.
+ */
 function getFlatRules(configObject) {
   const flatConfig = getFlatConfig(configObject);
 
-  return flatConfig.reduce((flatRules, config) => {
+  // Flatten the config array into a single object
+  const rawFlatRules = flatConfig.reduce((flatRules, config) => {
     if (RULES in config) {
       return {
         ...flatRules,
@@ -238,18 +315,29 @@ function getFlatRules(configObject) {
     }
     return flatRules;
   }, {});
+
+  // Sort the flat rules alphabetically and return them
+  return Object.keys(rawFlatRules)
+    .sort()
+    .reduce((flatRules, ruleName) => {
+      flatRules[ruleName] = rawFlatRules[ruleName];
+      return flatRules;
+    }, {});
 }
 
 /**
- * Takes a parsed eslint config object and flattens it and the configs it
- * extends into an array.
+ * Takes an eslint config object and flattens it and the configs it
+ * extends into a single array, ordered by their dependency relationships.
+ *
+ * @param {Record<string, unknown>} configObject - An eslint config object (e.g. .eslintrc.js).
+ * @returns {Record<string, unknown>[]} An array of parsed eslint config objects.
  */
 function getFlatConfig(configObject) {
   // FlatCompat does a lot of stuff under the hood, including resolving the
   // modules exporting the configs extended by the given config.
   // Luckily for us, that's kind of the hardest part.
   const flatConfig = new FlatCompat().config(configObject);
-  populateNamedRulesets(flatConfig);
+  populateRecommendedRules(flatConfig);
   return flatConfig;
 }
 
@@ -257,10 +345,13 @@ function getFlatConfig(configObject) {
  * getFlatConfig helper.
  * Looks for for the string 'eslint:recommended' in the given config array
  * and replaces it with its corresponding rules object.
+ * Mutates the given array in place.
  *
  * Throws an error if the config array contains an invalid config object.
+ *
+ * @param {Record<string, unknown>[]} configArray - A flat eslint config array.
  */
-function populateNamedRulesets(configArray) {
+function populateRecommendedRules(configArray) {
   let index = null;
   while (index !== -1) {
     index = configArray.indexOf(ESLINT_RECOMMENDED);
