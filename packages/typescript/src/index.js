f30711b60c0a58dc9edb82eaf32ead1d7893075f
Erik Marks <rekmarks@protonmail.com>
2021-04-01 15:56:11 -0700

    Delete some extraneous TypeScript rule

--- packages/typescript/src/index.js
+++ packages/typescript/src/index.js
@@ -21,26 +21,6 @@ module.exports = {
     // Should be disabled per Prettier
     '@typescript-eslint/no-extra-semi': 'off',
 
-    // Checked by TypeScript - ts(2378)
-    'getter-return': 'off',
-    // Checked by TypeScript - ts(2300)
-    'no-dupe-args': 'off',
-    // Checked by TypeScript - ts(1117)
-    'no-dupe-keys': 'off',
-    // Checked by TypeScript - ts(7027)
-    'no-unreachable': 'off',
-    // Checked by TypeScript - ts(2367)
-    'valid-typeof': 'off',
-    // Checked by TypeScript - ts(2588)
-    'no-const-assign': 'off',
-    // Checked by TypeScript - ts(2588)
-    'no-new-symbol': 'off',
-    // Checked by TypeScript - ts(2376)
-    'no-this-before-super': 'off',
-    // Checked by TypeScript - `strictNullChecks`
-    'no-undef': 'off',
-    // Checked by TypeScript
-    'no-redeclare': 'off',
     // Checked by TypeScript
     'import/no-unresolved': 'off',
 
