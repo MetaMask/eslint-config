ed5d5a8d73eba3b17dac28795cdd5cc25cabc26e
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-11-14 21:28:16 +0100

    Fix tests

--- packages/typescript/src/index.test.js
+++ packages/typescript/src/index.test.js
@@ -1,4 +1,5 @@
 const { ESLint } = require('eslint');
+const { resolve } = require('path');
 
 const config = require('.');
 
@@ -12,9 +13,18 @@ describe('index', () => {
         },
       },
       useEslintrc: false,
+      overrideConfig: {
+        parserOptions: {
+          tsconfigRootDir: resolve(__dirname, '..'),
+          project: 'tsconfig.json',
+        },
+      },
     });
 
-    const result = await api.lintText(`console.log('Hello, world!');\n`);
+    // In order to test rules that require type information, we need to actually
+    // compile the file with TypeScript, so rather than using `api.lintText()`,
+    // we use `api.lintFiles()` and pass in a file that we know will pass.
+    const result = await api.lintFiles(resolve(__dirname, '__test__/dummy.ts'));
 
     expect(result[0].messages).toStrictEqual([]);
     expect(result[0].warningCount).toBe(0);
