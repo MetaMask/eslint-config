c97444ac23fd2d3bc94e4ca4cb5389e3337ceb50
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-11-14 21:09:56 +0100

    Use existing no-undef and import/no-nodejs-modules rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
 
   rules: {
     'import/no-dynamic-require': 'off',
+    'import/no-nodejs-modules': 'off',
     'node/global-require': 'off',
     'node/no-process-exit': 'off',
     'node/no-sync': 'off',
