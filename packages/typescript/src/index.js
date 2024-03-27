3a827429be5179543e78e3b806c4d2b60178a7fa
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-03-27 13:54:39 +0100

    Update @typescript-eslint/eslint-plugin recommended rules name

--- packages/typescript/src/index.js
+++ packages/typescript/src/index.js
@@ -27,7 +27,7 @@ module.exports = {
 
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:import/typescript',
   ],
 
