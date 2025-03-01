6209348c27148a4ed475e389d692728c9987b27b
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-03-01 12:44:03 +0100

    Enable `jest/unbound-method`

--- packages/jest/src/index.mjs
+++ packages/jest/src/index.mjs
@@ -18,7 +18,6 @@ const config = createConfig({
 
   rules: {
     '@typescript-eslint/unbound-method': 'off',
-
     'jest/consistent-test-it': ['error', { fn: 'it' }],
     'jest/no-duplicate-hooks': 'error',
     'jest/no-test-return-statement': 'error',
@@ -29,6 +28,7 @@ const config = createConfig({
     'jest/prefer-todo': 'error',
     'jest/require-top-level-describe': 'error',
     'jest/require-to-throw-message': 'error',
+    'jest/unbound-method': 'error',
     'jest/valid-expect': ['error', { alwaysAwait: true }],
     'jest/no-restricted-matchers': [
       'error',
