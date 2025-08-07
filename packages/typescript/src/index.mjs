cd9425a97780e796ab8060e618a34a0788b70d8e
Mark Stacey <markjstacey@gmail.com>
2025-08-07 11:02:35 -0230

    Revert "feat(typescript): Update type import specifier rules" (#407)
    Reverts MetaMask/eslint-config#381 temporarily because it includes
    breaking changes, and we'd like to release the non-breaking changes on
    `main` without a major version bump. This will be restored after
    release.

--- packages/typescript/src/index.mjs
+++ packages/typescript/src/index.mjs
@@ -38,10 +38,14 @@ const config = createConfig({
   },
 
   rules: {
+    // Handled by TypeScript
+    'import-x/no-unresolved': 'off',
+
     // Our rules
     '@typescript-eslint/array-type': 'error',
     '@typescript-eslint/consistent-type-assertions': 'error',
     '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-imports': 'error',
     '@typescript-eslint/explicit-function-return-type': 'error',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-namespace': [
@@ -194,15 +198,6 @@ const config = createConfig({
 
     'no-useless-constructor': 'off',
     '@typescript-eslint/no-useless-constructor': 'error',
-
-    /* import-x plugin rules */
-
-    // Handled by TypeScript
-    'import-x/no-unresolved': 'off',
-
-    // Combined with the "verbatimModuleSyntax" tsconfig option, a better option than
-    // @typescript-eslint/consistent-type-imports
-    'import-x/consistent-type-specifier-style': ['error', 'prefer-top-level'],
 
     /* jsdoc plugin rules */
 
