c832feb995a5d5d785510832504b34a29e2227d7
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-08-07 15:11:59 +0200

    Disable `@typescript-eslint/unbound-method` rule in Jest tests (#391)
    This disables the `@typescript-eslint/unbound-method` rule in tests,
    which is too strict in cases, for example when asserting that a function
    has been called:
    
    ```ts
    const someObject = {
      foo: function() {
        // ...
      }
    };
    
    // Assuming this is mocked somewhere.
    expect(someObject.foo).toHaveBeenCalled();
    ```

---
+++ packages/jest/src/__test__/dummy.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('dummy test file', () => {
+  it('does nothing', () => {
+    expect(true).toBe(true);
+  });
+});
--- packages/jest/src/index.test.mjs
+++ packages/jest/src/index.test.mjs
@@ -1,4 +1,6 @@
 import { ESLint } from 'eslint';
+import globals from 'globals';
+import { resolve } from 'path';
 import { describe, it, expect } from 'vitest';
 
 import config from './index.mjs';
@@ -10,6 +12,41 @@ describe('index', () => {
     });
 
     const result = await api.lintText(`export {};\n`);
+
+    expect(result[0].messages).toStrictEqual([]);
+    expect(result[0].warningCount).toBe(0);
+    expect(result[0].errorCount).toBe(0);
+  });
+
+  it('is a valid ESLint config for TypeScript files', async () => {
+    const api = new ESLint({
+      baseConfig: config,
+      overrideConfig: {
+        rules: {
+          // This rule causes an error when importing `describe`, `it`, `expect`
+          // from `vitest` in the `__test__/dummy.test.ts` file. It's not really
+          // relevant to this test, so we disable it here.
+          '@typescript-eslint/no-shadow': 'off',
+        },
+
+        languageOptions: {
+          globals: {
+            ...globals.node,
+          },
+          parserOptions: {
+            tsconfigRootDir: resolve(import.meta.dirname, '..'),
+            project: 'tsconfig.json',
+          },
+        },
+      },
+    });
+
+    // In order to test rules that require type information, we need to actually
+    // compile the file with TypeScript, so rather than using `api.lintText()`,
+    // we use `api.lintFiles()` and pass in a file that we know will pass.
+    const result = await api.lintFiles(
+      resolve(import.meta.dirname, '__test__/dummy.test.ts'),
+    );
 
     expect(result[0].messages).toStrictEqual([]);
     expect(result[0].warningCount).toBe(0);
